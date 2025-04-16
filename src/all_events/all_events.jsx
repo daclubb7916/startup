01f4a3039b12105376708d61392157a2c346c856
Dakota Clubb <dakota.clubb@gmail.com>
2025-04-16 03:08:06 -0600

    added jsx files

---
+++ src/all_events/all_events.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function All_Events() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>all_events displayed here</div>
+    </main>
+  );
+}
---
+++ src/my_events/my_events.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function My_Events() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>my_events displayed here</div>
+    </main>
+  );
+}
